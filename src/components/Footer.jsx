06de377a453811514d485ac315dc149397b8156f
Rhamon Guedes <rhamon.guedes@al.infnet.edu.br>
2023-01-21 20:40:15 -0300

    set up of details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <div className={styles.footer}>
-        <p>Todos os direitos reservados <span>&#169;</span> 2023</p>
+        <p>InovaMed - Todos os direitos reservados <span>&#169;</span> 2023</p>
     </div>
   )
 }
